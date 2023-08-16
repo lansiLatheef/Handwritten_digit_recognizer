a4ac2e8dbfc10a0240ee2d958c970a97b010dbad
Lansi H <92106186+lansiLatheef@users.noreply.github.com>
2023-08-16 15:27:54 +0530

    Update digit_recognizer.py

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -1,7 +1,6 @@
 from keras.models import load_model
 from tkinter import *
 import tkinter as tk
-import win32gui
 from PIL import ImageGrab, Image
 import numpy as np
 
@@ -25,40 +24,43 @@ class App(tk.Tk):
         tk.Tk.__init__(self)
 
         self.x = self.y = 0
-        
+
         # Creating elements
         self.canvas = tk.Canvas(self, width=300, height=300, bg = "white", cursor="cross")
         self.label = tk.Label(self, text="Draw..", font=("Helvetica", 48))
-        self.classify_btn = tk.Button(self, text = "Recognise", command = self.classify_handwriting)   
+        self.classify_btn = tk.Button(self, text = "Recognise", command = self.classify_handwriting)
         self.button_clear = tk.Button(self, text = "Clear", command = self.clear_all)
-       
+
         # Grid structure
         self.canvas.grid(row=0, column=0, pady=2, sticky=W, )
         self.label.grid(row=0, column=1,pady=2, padx=2)
         self.classify_btn.grid(row=1, column=1, pady=2, padx=2)
         self.button_clear.grid(row=1, column=0, pady=2)
-        
+
         #self.canvas.bind("<Motion>", self.start_pos)
         self.canvas.bind("<B1-Motion>", self.draw_lines)
 
     def clear_all(self):
         self.canvas.delete("all")
-        
+
     def classify_handwriting(self):
-        HWND = self.canvas.winfo_id()  # get the handle of the canvas
-        rect = win32gui.GetWindowRect(HWND)  # get the coordinate of the canvas
-        a,b,c,d = rect
-        rect=(a+4,b+4,c-4,d-4)
-        im = ImageGrab.grab(rect)
+        x = self.winfo_rootx() + self.canvas.winfo_x()
+        y = self.winfo_rooty() + self.canvas.winfo_y()
+        x1 = x + self.canvas.winfo_width()
+        y1 = y + self.canvas.winfo_height()
+        rect = (x, y, x1, y1)
+        
+        im = ImageGrab.grab(bbox=rect)
 
         digit, acc = predict_digit(im)
         self.label.configure(text= str(digit)+', '+ str(int(acc*100))+'%')
+
 
     def draw_lines(self, event):
         self.x = event.x
         self.y = event.y
         r=8
         self.canvas.create_oval(self.x-r, self.y-r, self.x + r, self.y + r, fill='black')
-       
+
 app = App()
 mainloop()
